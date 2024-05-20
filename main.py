1530aed650e85d904dfccb5a1a55445553d80847
Mixss <michalziemiec@wp.pl>
2024-05-20 22:50:18 +0200

    started buying offer test
    Signed-off-by: Mixss <michalziemiec@wp.pl>

--- main.py
+++ main.py
@@ -9,7 +9,6 @@ driver.get("http://localhost:3000")
 driver.implicitly_wait(5)
 
 
-
 def do_search_query():
     # destinations
     driver.find_element(by=By.XPATH, value="/html/body/div[1]/div[2]/div[2]/div/button[1]").click()
@@ -26,11 +25,13 @@ def do_search_query():
     # when from select
     driver.find_element(by=By.XPATH, value="/html/body/div[2]/div/div/div[1]/div/div/button").click()
     # when from select date
-    driver.find_element(by=By.XPATH, value="/html/body/div[3]/div[2]/div/div/div/div[2]/div/div/div[2]/div/div[1]/button[1]").click()
+    driver.find_element(by=By.XPATH,
+                        value="/html/body/div[3]/div[2]/div/div/div/div[2]/div/div/div[2]/div/div[1]/button[1]").click()
     # when to select
     driver.find_element(by=By.XPATH, value="/html/body/div[2]/div/div/div[2]/div/div/button").click()
     # when to select date
-    driver.find_element(by=By.XPATH, value="/html/body/div[3]/div[2]/div/div/div/div[2]/div/div/div[2]/div/div[2]/button[1]").click()
+    driver.find_element(by=By.XPATH,
+                        value="/html/body/div[3]/div[2]/div/div/div/div[2]/div/div/div[2]/div/div[2]/button[1]").click()
     # from
     driver.find_element(by=By.XPATH, value="/html/body/div[1]/div[2]/div[2]/div/button[4]").click()
     # from options
@@ -44,8 +45,17 @@ def do_search_query():
     # driver.find_element(by=By.XPATH, value="").click()
 
 
+def assert_3_offers():
+    listing = driver.find_element(by=By.XPATH, value="/html/body/div/div[2]").find_elements(by=By.TAG_NAME, value="div")
+    assert len(listing) == 3
 
+
+def buy_offer():
+    driver.find_element(by=By.XPATH, value="/html/body/div/div[2]/div[3]/div[3]/a/button").click()
 
 
 do_search_query()
+assert_3_offers()
+buy_offer()
+
 time.sleep(5)
