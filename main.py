370c4c87b8351bd852b684b88227397591bf7241
Paweł Cichowski <pcichowski@pcichowski.com>
2024-06-10 10:52:40 +0200

    main.py: add offer searching
    Signed-off-by: Paweł Cichowski <pcichowski@pcichowski.com>

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.chrome.service import Service
 import time
 
-driver = webdriver.Chrome()
-driver.get("http://localhost:3000")
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+driver = webdriver.Firefox()
+driver.get("http://localhost:18453")
+driver.maximize_window()
 driver.implicitly_wait(5)
 
 
@@ -16,10 +16,13 @@ def do_search_query():
     driver.find_element(by=By.XPATH, value="/html/body/div[2]/div/div/div/div/div/label[14]/span[1]/input").click()
     driver.find_element(by=By.XPATH, value="/html/body/div[2]/div/div/div/div/div/label[11]/span[1]/input").click()
     driver.find_element(by=By.XPATH, value="/html/body/div[2]/div/div/div/div/div/label[7]/span[1]/input").click()
+    driver.find_element(by=By.XPATH, value="/html/body/div[2]/div/div/div/div/div/label[4]/span[1]/input").click()
+    driver.find_element(by=By.XPATH, value="/html/body/div[2]/div/div/div/div/div/label[3]/span[1]/input").click()
     # guests
     driver.find_element(by=By.XPATH, value="/html/body/div[1]/div[2]/div[2]/div/button[2]").click()
     # guests options
     driver.find_element(by=By.XPATH, value="/html/body/div[2]/div/div/div[2]/div/button[2]").click()
+
     # when
     driver.find_element(by=By.XPATH, value="/html/body/div[1]/div[2]/div[2]/div/button[3]").click()
     # when from select
@@ -27,22 +30,28 @@ def do_search_query():
     # when from select date
     driver.find_element(by=By.XPATH,
                         value="/html/body/div[3]/div[2]/div/div/div/div[2]/div/div/div[2]/div/div[1]/button[1]").click()
+
+    time.sleep(0.2)
     # when to select
     driver.find_element(by=By.XPATH, value="/html/body/div[2]/div/div/div[2]/div/div/button").click()
     # when to select date
     driver.find_element(by=By.XPATH,
                         value="/html/body/div[3]/div[2]/div/div/div/div[2]/div/div/div[2]/div/div[2]/button[1]").click()
-    # from
-    driver.find_element(by=By.XPATH, value="/html/body/div[1]/div[2]/div[2]/div/button[4]").click()
-    # from options
-    driver.find_element(by=By.XPATH, value="/html/body/div[2]/div/div/div[1]/div[2]/label[3]/span[1]/input").click()
-    driver.find_element(by=By.XPATH, value="/html/body/div[2]/div/div/div[1]/div[2]/label[4]/span[1]/input").click()
-    driver.find_element(by=By.XPATH, value="/html/body/div[1]/div[2]/div[2]/div/button[5]").click()
 
-    # driver.find_element(by=By.XPATH, value="").click()
-    # driver.find_element(by=By.XPATH, value="").click()
-    # driver.find_element(by=By.XPATH, value="").click()
-    # driver.find_element(by=By.XPATH, value="").click()
+    driver.find_element(by=By.XPATH, value='/html/body/div/div[2]/div[1]/h1').click()
+
+    # from
+    driver.find_element(by=By.XPATH, value="/html/body/div/div[2]/div[2]/div/button[4]").click()
+    # from options
+    driver.find_element(by=By.XPATH, value="/html/body/div[2]/div/div/div[1]/div[2]/label[4]/span[1]/input").click()
+    driver.find_element(by=By.XPATH, value="/html/body/div[2]/div/div/div[2]/div[2]/div/label[4]/span[1]/input").click()
+    driver.find_element(by=By.XPATH, value="/html/body/div[2]/div/div/div[1]/div[2]/label[6]/span[1]/input").click()
+    driver.find_element(by=By.XPATH, value="/html/body/div/div[2]/div[2]/div/button[5]").click()
+
+
+def refresh_offers():
+    driver.find_element(by=By.XPATH, value="/html/body/div/div[2]/div[1]/div[1]/button[5]").click()
+    time.sleep(10)
 
 
 def assert_3_offers():
@@ -54,8 +63,9 @@ def buy_offer():
     driver.find_element(by=By.XPATH, value="/html/body/div/div[2]/div[3]/div[3]/a/button").click()
 
 
-do_search_query()
-assert_3_offers()
-buy_offer()
+if __name__ == '__main__':
+    do_search_query()
+    time.sleep(5)
 
-time.sleep(5)
+    while True:
+        refresh_offers()
